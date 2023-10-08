fd42563e8778beffc74a9aa6cdbb9e4e24115d2b
amirhosein-ziaei <amir.ziaei1379@gmail.com>
2023-10-08 15:33:27 +0330

    add docstring for vertical_flip function

--- data_augmentation/Geometric_Augmentations.py
+++ data_augmentation/Geometric_Augmentations.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 from tensorflow import Tensor
 from typing import Tuple
 
-def horizontal_flip(image: Tensor, mask: Tensor, probability: int = 0.5) -> Tuple[Tensor, Tensor]:
+
+def horizontal_flip(image: Tensor, mask: Tensor, probability: float = 0.5) -> Tuple[Tensor, Tensor]:
     """
-    Apply horizontal flip data augmentation to a car image and its corresponding mask.
+    Apply horizontal flip data augmentation to an input image and its corresponding mask.
 
     Args:
-        image (Tensor): The car image tensor.
-        mask (Tensor): The mask image tensor.
-        probability (int, optional): The probability of applying the horizontal flip. Defaults to 0.5.
-
+        image (Tensor): The input image tensor.
+        mask (Tensor): The input mask tensor.
+        probability (float, optional): The probability of applying the horizontal flip. Defaults to 0.5.
 
     Returns:
-        Tuple[Tensor, Tensor]: A tuple containing the horizontally flipped car image and mask image.
+        Tuple[Tensor, Tensor]: A tuple containing the horizontally flipped image and mask tensors.
     """
+    
     flip_prob = tf.random.uniform(())
     
     if flip_prob > probability:
@@ -24,11 +25,25 @@ def horizontal_flip(image: Tensor, mask: Tensor, probability: int = 0.5) -> Tupl
     return flipped_image, flipped_mask
 
 
-def vertical_flip(image: Tensor, mask: Tensor, probability: int = 0.5) -> Tuple[Tensor, Tensor]:
+def vertical_flip(image: Tensor, mask: Tensor, probability: float = 0.5) -> Tuple[Tensor, Tensor]:
+    """
+    Perform vertical flip (up-down flip) on the input image and mask tensors using TensorFlow.
+
+    Args:
+        image (Tensor): The input image tensor.
+        mask (Tensor): The input mask tensor.
+        probability (float, optional): The probability of applying the vertical flip. Defaults to 0.5, which means a 50% chance of flipping.
+
+    Returns:
+        Tuple[Tensor, Tensor]: A tuple containing the vertically flipped image and mask tensors.
+    """
+    
     flip_prob = tf.random.uniform(())
 
     if flip_prob > probability:
         flipped_image = tf.image.flip_up_down(image)
         flipped_mask = tf.image.flip_up_down(mask)
 
-    return flipped_image, flipped_mask
+        return flipped_image, flipped_mask
+
+    return image, mask
