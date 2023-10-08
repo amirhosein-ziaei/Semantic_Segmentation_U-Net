a03b62b3d2f4776bc3b9bc9940e629b30dd60392
amirhosein-ziaei <amir.ziaei1379@gmail.com>
2023-10-08 15:20:28 +0330

    add docstring for horizontal_flip function

--- data_augmentation/Geometric_Augmentations.py
+++ data_augmentation/Geometric_Augmentations.py
@@ -3,6 +3,16 @@ from tensorflow import Tensor
 from typing import Tuple
 
 def horizontal_flip(image: Tensor, mask: Tensor) -> Tuple[Tensor, Tensor]:
+    """
+    Apply horizontal flip data augmentation to a car image and its corresponding mask.
+
+    Args:
+        image (Tensor): The car image tensor.
+        mask (Tensor): The mask image tensor.
+
+    Returns:
+        Tuple[Tensor, Tensor]: A tuple containing the horizontally flipped car image and mask image.
+    """
     flip_prob = tf.random.uniform(())
     
     if flip_prob > 0.5:
